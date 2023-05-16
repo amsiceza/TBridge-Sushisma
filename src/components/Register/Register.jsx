238314535fdc6d2a39ca68744ad22da6a268d9b2
[amsiceza] <[ismacerverazamora@gmail.com]>
2023-05-16 20:20:44 +0200

    Add register fixed

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,48 +2,44 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import "./Register.scss"
+const API_URL = 'http://localhost:3000'
+
 
 
 const Register = () => {
-  const [formData, setFormData] = useState({
-    first_name: '',
-    last_name: '',
-    username: '',
-    email: '',
-    password: '',
-  });
-
-  const { first_name, last_name, username, email, password } = formData;
-
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-
-      const body = JSON.stringify({ name, email, password });
-
-      const response = await axios.post(
-        'http://localhost:300/users/create',
-        body,
-        config
-      );
-
-      console.log('Usuario registrado exitosamente', response.data);
-      navigate('/login'); // Redirigir a la página de login
-    } catch (err) {
-      console.error('Error al registrar el usuario', err.response.data);
-    }
-  };
-
-  const navigate = useNavigate(); // Obtener la función navigate
+    const navigate = useNavigate()
+    const [formValues, setFormValues] = useState({
+      first_name: '',
+      last_name: '',
+      username: '',
+      password: '',
+      email: '',
+    });
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+  
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setFormValues({ ...formValues, [name]: value });
+    };
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      setIsLoading(true);
+      setError('');
+      try {
+        const response = await axios.post(API_URL+'/users/create', formValues);
+        console.log(response.data);
+        setIsLoading(false);
+        navigate('/login')
+  
+      } catch (error) {
+        console.error(error);
+        setError(error.message);
+        setIsLoading(false);
+      }
+    };
+  
 
   return (
     
@@ -52,48 +48,53 @@ const Register = () => {
           <input
             type="text"
             name="first_name"
-            value={first_name}
-            onChange={(e) => onChange(e)}
-            placeholder="First name"
+            value={formValues.first_name}
+            onChange={handleInputChange}
+            placeholder="Enter your first name"
+            autoComplete='off'
           />
         </div>
         <div>
           <input
             type="text"
             name="last_name"
-            value={last_name}
-            onChange={(e) => onChange(e)}
-            placeholder="Last name"
+            value={formValues.last_name}
+            onChange={handleInputChange}
+            placeholder="Enter your last name"
+            autoComplete='off'
           />
         </div>
         <div>
           <input
             type="text"
             name="username"
-            value={username}
-            onChange={(e) => onChange(e)}
-            placeholder="Username"
+            value={formValues.username}
+            onChange={handleInputChange}
+            placeholder="Enter your username"
+            autoComplete='off'
           />
         </div>
         <div>
           <input
             type="text"
             name="email"
-            value={email}
-            onChange={(e) => onChange(e)}
-            placeholder="Your email"
+            value={formValues.email}
+            onChange={handleInputChange}
+            placeholder="Enter your email"
+            autoComplete='off'
           />
         </div>
         <div>
           <input
             type="password"
             name="password"
-            value={password}
-            onChange={(e) => onChange(e)}
-            placeholder="Password"
+            value={formValues.password}
+            onChange={handleInputChange}
+            placeholder="Enter your password"
+            autoComplete='off'
           />
         </div>
-        <button className="submit-add-procut" type="submit">Registrarse</button>
+        <button className="submit-add-procut" type="submit" onClick={handleSubmit}>Registrarse</button>
       </form>
     
   );
